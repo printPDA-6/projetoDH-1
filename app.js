e43607c6a54b63a8a0304e5e0eb2e82cdd16f800
printAllen-Lee <printallenlee@gmail.com>
2022-03-27 08:28:40 -0300

    app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/',(req, res) => {
     res.send('quero um livro')
 })
 
+app.get('/sobre',(req,res) =>{
+    res.send('Bem-vindo!')
+})
+
 app.listen(port, function() {
     console.log ('Example app listening on port '+ port)
 })
