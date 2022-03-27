4a92e18f7c0900b2bfe69b86b6726d79b8796a96
printAllen-Lee <printallenlee@gmail.com>
2022-03-27 12:28:10 -0300

    app.js com o express ativo!

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3200;
+const port = 8080;
 
 app.get('/',(req, res) => {
     res.send('quero um livro')
@@ -10,8 +10,8 @@ app.get('/sobre',(req,res) =>{
     res.send('Bem-vindo!')
 })
 
+//Rafael seu codigo aqui!
+
 app.listen(port, function() {
-    console.log ('Example app listening on port '+ port)
+    console.log ('Express on!'+ port)
 })
-
-
