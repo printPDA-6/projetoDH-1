86773cecd556d32e3ea746f0295788e731e4f0cf
printAllen-Lee <printallenlee@gmail.com>
2022-05-10 19:37:27 -0300

    pag login and buscar

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const res = require('express/lib/response');
-const path = require('path');
 const app = express();
-const port = 8080;
-
 
 app.get('/',(req,res) =>{
+
     res.sendFile(__dirname + '/public/index.html');
 })
 
 
 
-app.listen(port , function() {
-    console.log ('Servidor funcionando na Url:'+"http://localhost:"+port)
-})
+app.listen(8080 , ()=> {
+    console.log ('Servidor funcionando na Url:'+"http://localhost:8080")
+});
