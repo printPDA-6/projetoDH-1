1fad71f5672df7ddd7e58333db787d20c939f2ad
Allen Lee <printallenlee@gmail.com>
2022-05-27 21:04:25 -0300

    page-web-config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 9000
+const port = 1717;
 
 
 app.set("view engine" , "ejs");
@@ -9,8 +9,16 @@ app.get('/',(req,res) =>{
   res.render("pages/index.html")
 })
 
+app.get('login',(req,res) =>{
+  res.render("pages/login.html")
+})
+
+app.get('buscar' , (req,res) =>{
+  res.render("pages/buscar.html")
+})
+
 
 
 app.listen(port , ()=> {
-    console.log ('Servidor funcionando na Url:'+"http://localhost:"+port)
+    console.log ('Servidor funcionando na Url:'+ "localhost:"+ port)
 });
