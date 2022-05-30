97e721f763e7d103d921ca14085ed81b2ec5e964
Rafael de sousa <sousarafael151@gmail.com>
2022-05-30 17:58:55 -0300

    alterando buscar

--- app.js
+++ app.js
@@ -2,25 +2,17 @@ const express = require('express');
 const app = express();
 const port = 1717;
 
-
+app.use(express.static('public/'))
 app.set("view engine" , "ejs");
 
-app.use(express.static(__dirname +'/public'));
-
 app.get('/',(req,res) =>{
-  res.render("pages/index.html")
+  res.render("pages/index")
 })
 
-app.get('login',(req,res) =>{
-  res.render("pages/login.html")
+app.get('/buscar', (req, res)=>{
+  return res.render('pages/buscar')
 })
-
-app.get('buscar' , (req,res) =>{
-  res.render("pages/buscar.html")
-})
-
-
 
 app.listen(port , ()=> {
-    console.log ('Servidor funcionando na Url:'+ "localhost:"+ port)
+    console.log (`Servidor Rodando! Porta: ${port}`)
 });
