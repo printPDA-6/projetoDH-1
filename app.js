60107db91e7d33391df1ea027989e10b43d2884d
Allen Lee <printallenlee@gmail.com>
2022-06-06 15:39:47 -0300

    add new pages

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const port = 1717;
 
 app.use(express.static('public/'))
+
 app.set("view engine" , "ejs");
 
 app.get('/',(req,res) =>{
-  res.render("pages/index")
+  return res.render("pages/index")
 })
 
 app.get('/buscar', (req, res)=>{
   return res.render('pages/buscar')
 })
+
+app.get('/login', (req, res)=>{
+  return res.render('pages/login')
+})
+
+
 
 app.listen(port , ()=> {
     console.log (`Servidor Rodando! Porta: ${port}`)
